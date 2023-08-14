022d50b78eb7b12fdcae3ef6050f93d121839a51
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-14 18:01:40 +0100

    setting class and text conditionally

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -2,12 +2,12 @@ import pizzas from "./pizzas/spinaci.jpg";
 
 const Pizza = ({ pizzaObj }) => {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzas} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
